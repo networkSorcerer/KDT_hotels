784c62b321083679d213bafcd931c51198be355a
networkSorcerer <gigattfyhig@gmail.com>
2024-10-23 13:46:17 +0900

    kdt mini project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static void menuSelect() {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
